b0cc0a38d2abef5eeeb74462fa5421314552139f
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-08 14:24:01 +0800

    Add load chat func

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadChat()
+        
+    }
+    
+    func loadChat() {
+        print("Loading Chat....")
     }
 
     override func didReceiveMemoryWarning() {
