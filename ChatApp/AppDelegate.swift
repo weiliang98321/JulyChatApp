a883dfe612a594430855c48751101e72b08a7c26
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-08 14:16:03 +0800

    Chg Storyboard to Auth Storyboard

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -17,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //get the vc
+        let authStoryboard = UIStoryboard(name: "Auth", bundle: Bundle.main)
+        let loginVC = authStoryboard.instantiateViewController(withIdentifier: "AuthNavigation")
+        
+        window?.rootViewController = loginVC
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
